1d7463e9b7c9d2e0877f1c9dbda92e0b3abbd0ac
Rafal Mielniczuk <rafal.mielniczuk2@gmail.com>
2023-02-28 21:35:28 +0000

    dummy port

---
+++ doomgeneric/doomgeneric_cykusz.c
@@ -0,0 +1,48 @@
+//doomgeneric for soso os
+#include <stdint.h>
+
+
+static unsigned char convertToDoomKey(unsigned char scancode)
+{
+}
+
+static void addKeyToQueue(int pressed, unsigned char keyCode)
+{
+}
+
+void disableRawMode()
+{
+}
+
+void enableRawMode()
+{
+}
+
+void DG_Init()
+{
+}
+
+static void handleKeyInput()
+{
+}
+
+void DG_DrawFrame()
+{
+}
+
+void DG_SleepMs(uint32_t ms)
+{
+}
+
+uint32_t DG_GetTicksMs()
+{
+    return 0;
+}
+
+int DG_GetKey(int* pressed, unsigned char* doomKey)
+{
+}
+
+void DG_SetWindowTitle(const char * title)
+{
+}
