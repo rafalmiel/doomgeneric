f36d1482a8e45f4b790c9cc21080b5a2816d3d0f
Rafal Mielniczuk <rafal.mielniczuk2@gmail.com>
2023-02-28 23:53:26 +0000

    Initial gfx output

--- doomgeneric/doomgeneric_cykusz.c
+++ doomgeneric/doomgeneric_cykusz.c
@@ -1,25 +1,163 @@
 //doomgeneric for soso os
-#include <stdint.h>
 
+#include "doomkeys.h"
+#include "m_argv.h"
+#include "doomgeneric.h"
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+
+#include <termios.h>
+
+#include <cykusz/syscall.h>
+
+static int FrameBufferFd = -1;
+static int* FrameBuffer = 0;
+
+static int KeyboardFd = -1;
+
+#define KEYQUEUE_SIZE 16
+
+static unsigned short s_KeyQueue[KEYQUEUE_SIZE];
+static unsigned int s_KeyQueueWriteIndex = 0;
+static unsigned int s_KeyQueueReadIndex = 0;
+
+static unsigned int s_PositionX = 0;
+static unsigned int s_PositionY = 0;
+
+static unsigned int s_ScreenWidth = 0;
+static unsigned int s_ScreenHeight = 0;
+
+enum EnFrameBuferIoctl
+{
+    FB_GET_WIDTH,
+    FB_GET_HEIGHT,
+    FB_GET_BITSPERPIXEL
+};
 
 static unsigned char convertToDoomKey(unsigned char scancode)
 {
+    unsigned char key = 0;
+
+    switch (scancode)
+    {
+    case 0x9C:
+    case 0x1C:
+        key = KEY_ENTER;
+        break;
+    case 0x01:
+        key = KEY_ESCAPE;
+        break;
+    case 0xCB:
+    case 0x4B:
+        key = KEY_LEFTARROW;
+        break;
+    case 0xCD:
+    case 0x4D:
+        key = KEY_RIGHTARROW;
+        break;
+    case 0xC8:
+    case 0x48:
+        key = KEY_UPARROW;
+        break;
+    case 0xD0:
+    case 0x50:
+        key = KEY_DOWNARROW;
+        break;
+    case 0x1D:
+        key = KEY_FIRE;
+        break;
+    case 0x39:
+        key = KEY_USE;
+        break;
+    case 0x2A:
+    case 0x36:
+        key = KEY_RSHIFT;
+        break;
+    case 0x15:
+        key = 'y';
+        break;
+    default:
+        break;
+    }
+
+    return key;
 }
 
 static void addKeyToQueue(int pressed, unsigned char keyCode)
 {
+	//printf("key hex %x decimal %d\n", keyCode, keyCode);
+
+        unsigned char key = convertToDoomKey(keyCode);
+
+        unsigned short keyData = (pressed << 8) | key;
+
+        s_KeyQueue[s_KeyQueueWriteIndex] = keyData;
+        s_KeyQueueWriteIndex++;
+        s_KeyQueueWriteIndex %= KEYQUEUE_SIZE;
 }
+
+
+struct termios orig_termios;
 
 void disableRawMode()
 {
+  //printf("returning original termios\n");
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
 void enableRawMode()
 {
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+  struct termios raw = orig_termios;
+  raw.c_lflag &= ~(ECHO);
+  raw.c_cc[VMIN] = 0;
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 void DG_Init()
 {
+    FrameBufferFd = open("/dev/fb", 0);
+
+    if (FrameBufferFd >= 0)
+    {
+        printf("Getting screen width...");
+        s_ScreenWidth = 640;
+        printf("%d\n", s_ScreenWidth);
+
+        printf("Getting screen height...");
+        s_ScreenHeight = 480;
+        printf("%d\n", s_ScreenHeight);
+
+        if (0 == s_ScreenWidth || 0 == s_ScreenHeight)
+        {
+            printf("Unable to obtain screen info!");
+            exit(1);
+        }
+
+        FrameBuffer = mmap(NULL, s_ScreenWidth * s_ScreenHeight * 4, PROT_READ | PROT_WRITE, 0, FrameBufferFd, 0);
+
+        if (FrameBuffer != (int*)-1)
+        {
+            printf("FrameBuffer mmap success\n");
+        }
+        else
+        {
+            printf("FrameBuffermmap failed\n");
+        }
+    }
+    else
+    {
+        printf("Opening FrameBuffer device failed!\n");
+    }
+
+    //enableRawMode();
 }
 
 static void handleKeyInput()
@@ -28,15 +166,25 @@ static void handleKeyInput()
 
 void DG_DrawFrame()
 {
+    if (FrameBuffer)
+    {
+        for (int i = 0; i < DOOMGENERIC_RESY; ++i)
+        {
+            memcpy(FrameBuffer + s_PositionX + (i + s_PositionY) * s_ScreenWidth, DG_ScreenBuffer + i * DOOMGENERIC_RESX, DOOMGENERIC_RESX * 4);
+        }
+    }
 }
 
 void DG_SleepMs(uint32_t ms)
 {
+    syscalln1(SYS_SLEEP, (uint64_t)ms * 1000000);
 }
 
 uint32_t DG_GetTicksMs()
 {
-    return 0;
+    uint32_t ticksMs = syscalln0(SYS_TICKSNS) / 1000000;
+    //printf("%d\n", ticksMs);
+    return ticksMs;
 }
 
 int DG_GetKey(int* pressed, unsigned char* doomKey)
