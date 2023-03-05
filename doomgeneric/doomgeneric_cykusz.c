d1bcc6531d05ed5ef79ccf9e3f2daacfaaad6462
Rafal Mielniczuk <rafal.mielniczuk2@gmail.com>
2023-03-05 15:28:03 +0000

    Add keyboard support to cykusz port

--- doomgeneric/doomgeneric_cykusz.c
+++ doomgeneric/doomgeneric_cykusz.c
@@ -8,6 +8,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/select.h>
+#include <termios.h>
+#include <stdlib.h>
+#include <sys/types.h>
 
 #include <sys/ioctl.h>
 #include <sys/mman.h>
@@ -33,50 +37,38 @@ static unsigned int s_PositionY = 0;
 static unsigned int s_ScreenWidth = 0;
 static unsigned int s_ScreenHeight = 0;
 
-enum EnFrameBuferIoctl
-{
-    FB_GET_WIDTH,
-    FB_GET_HEIGHT,
-    FB_GET_BITSPERPIXEL
-};
-
 static unsigned char convertToDoomKey(unsigned char scancode)
 {
     unsigned char key = 0;
 
     switch (scancode)
     {
-    case 0x9C:
     case 0x1C:
         key = KEY_ENTER;
         break;
     case 0x01:
         key = KEY_ESCAPE;
         break;
-    case 0xCB:
-    case 0x4B:
+    case 0x69:
         key = KEY_LEFTARROW;
         break;
-    case 0xCD:
-    case 0x4D:
+    case 0x6A:
         key = KEY_RIGHTARROW;
         break;
-    case 0xC8:
-    case 0x48:
+    case 0x67:
         key = KEY_UPARROW;
         break;
-    case 0xD0:
-    case 0x50:
+    case 0x6C:
         key = KEY_DOWNARROW;
         break;
     case 0x1D:
         key = KEY_FIRE;
         break;
-    case 0x39:
+    case 0x38:
         key = KEY_USE;
         break;
-    case 0x2A:
     case 0x36:
+    case 0x2A:
         key = KEY_RSHIFT;
         break;
     case 0x15:
@@ -91,15 +83,13 @@ static unsigned char convertToDoomKey(unsigned char scancode)
 
 static void addKeyToQueue(int pressed, unsigned char keyCode)
 {
-	//printf("key hex %x decimal %d\n", keyCode, keyCode);
+    unsigned char key = convertToDoomKey(keyCode);
 
-        unsigned char key = convertToDoomKey(keyCode);
+    unsigned short keyData = (pressed << 8) | key;
 
-        unsigned short keyData = (pressed << 8) | key;
-
-        s_KeyQueue[s_KeyQueueWriteIndex] = keyData;
-        s_KeyQueueWriteIndex++;
-        s_KeyQueueWriteIndex %= KEYQUEUE_SIZE;
+    s_KeyQueue[s_KeyQueueWriteIndex] = keyData;
+    s_KeyQueueWriteIndex++;
+    s_KeyQueueWriteIndex %= KEYQUEUE_SIZE;
 }
 
 
@@ -107,7 +97,6 @@ struct termios orig_termios;
 
 void disableRawMode()
 {
-  //printf("returning original termios\n");
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
@@ -115,9 +104,14 @@ void enableRawMode()
 {
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
+
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO);
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
   raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
@@ -157,11 +151,35 @@ void DG_Init()
         printf("Opening FrameBuffer device failed!\n");
     }
 
-    //enableRawMode();
+    KeyboardFd = open("/dev/kbd", O_RDONLY);
+
+    enableRawMode();
 }
+
+struct event {
+    uint16_t typ;
+    uint16_t code;
+    int32_t val;
+};
 
 static void handleKeyInput()
 {
+    fd_set r;
+    struct timeval tm;
+    tm.tv_sec = 0;
+    tm.tv_usec = 0;
+
+    FD_ZERO(&r);
+    FD_SET(KeyboardFd, &r);
+
+    int res = select(1, &r, 0, 0, &tm);
+
+    if (res > 0) {
+        struct event evt;
+        read(KeyboardFd, &evt, sizeof(struct event));
+
+        addKeyToQueue((evt.val == 1) ? 0 : 1, evt.code);
+    }
 }
 
 void DG_DrawFrame()
@@ -173,6 +191,8 @@ void DG_DrawFrame()
             memcpy(FrameBuffer + s_PositionX + (i + s_PositionY) * s_ScreenWidth, DG_ScreenBuffer + i * DOOMGENERIC_RESX, DOOMGENERIC_RESX * 4);
         }
     }
+
+    handleKeyInput();
 }
 
 void DG_SleepMs(uint32_t ms)
@@ -183,13 +203,28 @@ void DG_SleepMs(uint32_t ms)
 uint32_t DG_GetTicksMs()
 {
     uint32_t ticksMs = syscalln0(SYS_TICKSNS) / 1000000;
-    //printf("%d\n", ticksMs);
     return ticksMs;
 }
 
 int DG_GetKey(int* pressed, unsigned char* doomKey)
 {
-    return 0;
+    if (s_KeyQueueReadIndex == s_KeyQueueWriteIndex)
+    {
+        //key queue is empty
+
+        return 0;
+    }
+    else
+    {
+        unsigned short keyData = s_KeyQueue[s_KeyQueueReadIndex];
+        s_KeyQueueReadIndex++;
+        s_KeyQueueReadIndex %= KEYQUEUE_SIZE;
+
+        *pressed = keyData >> 8;
+        *doomKey = keyData & 0xFF;
+
+        return 1;
+    }
 }
 
 void DG_SetWindowTitle(const char * title)
